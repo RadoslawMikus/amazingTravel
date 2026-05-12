208f795082aab3b94832665ca1b8644d038dfeb8
davidsmith931 <radoslaw.mikus@gmail.com>
2026-05-12 21:21:49 +0200

    Modified components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+Promise.all([
+  fetch("components/navbar.html").then((res) => res.text()),
+  fetch("components/footer.html").then((res) => res.text()),
+]).then(([navbar, footer]) => {
+  document.querySelector("#navbar").innerHTML = navbar;
+  document.querySelector("#footer").innerHTML = footer;
+});
+
 document.addEventListener("click", (e) => {
   const hamburger = document.querySelector(".hamburger");
   const navbar = document.querySelector(".navbar");
